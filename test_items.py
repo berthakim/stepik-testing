9421ab85611df03d7c884e19dddb08fc4d22f070
berthakim <kte.ent@gmail.com>
2021-05-08 18:44:36 +0900

    Modify selector in order to make it more stable

--- test_items.py
+++ test_items.py
@@ -11,7 +11,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_button_should_exist(browser):
     browser.get(link)
     time.sleep(10)
-    assert browser.find_elements_by_css_selector(".btn-primary.btn-add-to-basket"), \
+    assert browser.find_elements_by_css_selector("#add_to_basket_form .btn-add-to-basket"), \
         "Button doesn't exist :("
 
     # if list will be empty function return [] which is False. Else --> True
