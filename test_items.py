0dc91621c4887ddccdd8b0e014c40a247603df76
berthakim <kte.ent@gmail.com>
2021-05-09 17:17:00 +0900

    Make error message more detailed

--- test_items.py
+++ test_items.py
@@ -3,6 +3,8 @@
 добавления в корзину
 """
 import time
+from selenium.common.exceptions import NoSuchElementException
+
 
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -10,8 +12,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_button_should_exist(browser):
     browser.get(link)
-    time.sleep(10)
-    assert browser.find_elements_by_css_selector("#add_to_basket_form .btn-add-to-basket"), \
-        "Button doesn't exist :("
-
-    # if list will be empty function return [] which is False. Else --> True
+    # time.sleep(30)  # comment out for take more time to see the page
+    buttons = len(browser.find_elements_by_css_selector("#add_to_basket_form .btn-add-to-basket"))
+    assert buttons == 1, "Button doesn't exist or There is two or more identical buttons :("
